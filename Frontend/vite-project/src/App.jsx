1191ddda1861a833120666b47c3a61d0760facbc
twilight05 <54307284+twilight05@users.noreply.github.com>
2024-08-22 14:14:52 +0100

    first commit

---
+++ Frontend/vite-project/src/App.jsx
@@ -0,0 +1,23 @@
+// src/App.jsx
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LandingPage from './components/LandingPage/LandingPage';
+import AdminPanel from './components/AdminPanel/AdminPanel';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        {/* Route for Landing Page */}
+        <Route path="/" element={<LandingPage />} />
+
+        {/* Route for Admin Panel */}
+        <Route path="/adminpanel" element={<AdminPanel />} />
+
+        {/* Redirect all other routes to the Landing Page */}
+        <Route path="*" element={<LandingPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
